e09625362f166801fc67f31b8236507ab3014941
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-06 04:54:21 +0300

    header all done

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "./header.css";
+import { nav } from "../../data/Data";
+import { Link } from "react-router-dom";
+
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [navList, setNavList] = useState(false);
+
+  return (
+    <>
+      <header>
+        <div className="container flex">
+          <div className="logo">
+            <img src="./images/logo.png" alt="" />
+          </div>
+          <div className="nav">
+            <ul className={navList ? "small" : "flex"}>
+              {nav.map((list, index) => (
+                <li key={index}>
+                  <Link to={list.path}>{list.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="toggle">
+            <button onClick={() => setNavList(!navList)}>
+              {navList ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
