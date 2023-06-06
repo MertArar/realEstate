2233d5f682762fd8b25566a182f5a339c780b40e
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-06 04:55:45 +0300

    Heading part added

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Heading = () => {
-  return <div>Heading</div>;
+const Heading = ({ title, subtitle }) => {
+  return (
+    <>
+      <div className="heading">
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+    </>
+  );
 };
 
 export default Heading;
