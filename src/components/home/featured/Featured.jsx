6bfa0a46419ab397635445e62804164f90ef41f9
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-07 03:14:01 +0300

    featured done

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Heading from "../../common/Heading";
+
+import "./featured.css";
+import FeaturedCard from "./FeaturedCard";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <>
+      <section className="featured background">
+        <div className="container">
+          <Heading
+            title="Featured Property Types"
+            subtitle="Find All Type of Property"
+          />
+          <FeaturedCard />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Featured;
